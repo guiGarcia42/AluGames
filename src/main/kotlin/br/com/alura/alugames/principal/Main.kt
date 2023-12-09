78bf2224cf3f18648dda9377c002b9440cf9545c
Guilherme Garcia <gui123.garcia@gmail.com>
2023-12-09 11:58:54 -0300

    Finishing course with project fully updated.

--- src/main/kotlin/br/com/alura/alugames/principal/Main.kt
+++ src/main/kotlin/br/com/alura/alugames/principal/Main.kt
@@ -1,50 +1,94 @@
 package br.com.alura.alugames.principal
 
+import br.com.alura.alugames.modelo.Gamer
 import br.com.alura.alugames.modelo.Jogo
 import br.com.alura.alugames.servicos.ConsumoApi
+import transformarEmIdade
 import java.util.Scanner
 
 fun main() {
 
     val leitura = Scanner(System.`in`)
-    println("Digite um código de jogo para buscar:")
-    val busca = leitura.nextLine()
+    val gamer = Gamer.criarGamer(leitura)
+    println("Cadastro concluído com sucesso. Dados do gamer:")
+    println(gamer)
+    println("Idade do gamer: " + gamer.dataNascimento?.transformarEmIdade())
 
-    // inicializamos a API para utilizar nos códigos
-    val buscaApi = ConsumoApi()
-    val informacaoJogo = buscaApi.buscaJogo(busca)
+    do {
+        println("Digite um código de jogo para buscar:")
+        val busca = leitura.nextLine()
 
-    //inicializando
-    var meuJogo: Jogo? = null
+        // inicializamos a API para utilizar nos códigos
+        val buscaApi = ConsumoApi()
+        val informacaoJogo = buscaApi.buscaJogo(busca)
 
-    //Tratamento de exceção KOTLIN, salvamos na variável e podemos tratar os erros separadamente
-    val resultado = runCatching {
-        meuJogo = Jogo(
-            informacaoJogo.info.title,
-            informacaoJogo.info.thumb
-        )
-    }
+        //inicializando
+        var meuJogo: Jogo? = null
 
-    // Somente no caso de falha no runCatch que iremos tratar.
-    resultado.onFailure {
-        println("Jogo inexistente. Tente outro id.")
-    }
-
-    // No caso de sucesso, seguir com esse bloco.
-    resultado.onSuccess {
-        println("Deseja inserir uma descrição personalizada? S/N")
-        val opcao = leitura.nextLine()
-        if(opcao.equals("s", true)) {
-            println("Insira a descrição personalizada para o jogo:")
-            val descricaoPersonalizada = leitura.nextLine()
-            meuJogo?.descricao = descricaoPersonalizada
-
-        }else{
-            meuJogo?.descricao = meuJogo?.titulo
+        //Tratamento de exceção KOTLIN, salvamos na variável e podemos tratar os erros separadamente
+        val resultado = runCatching {
+            meuJogo = Jogo(
+                informacaoJogo.info.title,
+                informacaoJogo.info.thumb
+            )
         }
+
+        // Somente no caso de falha no runCatch que iremos tratar.
+        resultado.onFailure {
+            println("Jogo inexistente. Tente outro id.")
+        }
+
+        // No caso de sucesso, seguir com esse bloco.
+        resultado.onSuccess {
+            println("Deseja inserir uma descrição personalizada? S/N")
+            val opcao = leitura.nextLine()
+            if(opcao.equals("s", true)) {
+                println("Insira a descrição personalizada para o jogo:")
+                val descricaoPersonalizada = leitura.nextLine()
+                meuJogo?.descricao = descricaoPersonalizada
+
+            }else{
+                meuJogo?.descricao = meuJogo?.titulo
+            }
+
+            gamer.jogosBuscados.add(meuJogo)
+        }
+
+        println("Deseja buscar um novo jogo? S/N")
+        val resposta = leitura.nextLine()
+
+    } while (resposta.equals("s", true))
+
+    println("Jogos buscados:")
+    println(gamer.jogosBuscados)
+
+    println("\n Jogos ordenados por título: ")
+
+    gamer.jogosBuscados.sortBy {
+        it?.titulo
     }
 
-    resultado.onSuccess {
-        println("Busca finalizada com sucesso.")
+    gamer.jogosBuscados.forEach {
+        println("Título: " + it?.titulo)
     }
+
+    val jogosFiltrados = gamer.jogosBuscados.filter {
+        it?.titulo?.contains("batman", true) ?: false
+    }
+
+    println("Deseja excluir algum jogo da lista original? S/N")
+    val opcao = leitura.nextLine()
+
+    if (opcao.equals("s", true)){
+        println(gamer.jogosBuscados)
+        println("\n Informe a posição do jogo que deseja excluir: ")
+        val posicao = leitura.nextInt()
+        gamer.jogosBuscados.removeAt(posicao)
+    }
+
+    println("\n Lista atualizada: ")
+    println(gamer.jogosBuscados)
+
+    println("Busca finalizada com sucesso.")
+
 }
